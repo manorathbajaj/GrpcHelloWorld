d540928715b4cca02f278b40a03c68803bf1ada6
Manorath Bajaj <manorathbajaj@gmail.com>
2020-05-25 07:47:08 -0400

    Create Client

---
+++ client/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"helloWorld/proto"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Printf("failed to connect to localhost")
+		panic(err)
+	}
+
+	client := proto.NewCRUDSreviceClient(conn)
+
+	g := gin.Default()
+	// Bad way of doing it but demo meh..
+	g.GET("/CREATE/:id/:name/:age/:address/:salary", func(ctx *gin.Context) {
+		id, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter id"})
+			return
+		}
+
+		age, err := strconv.ParseUint(ctx.Param("age"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter age"})
+			return
+		}
+
+		salary, err := strconv.ParseUint(ctx.Param("salary"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter salary"})
+			return
+		}
+
+		name := ctx.Param("name")
+		address := ctx.Param("address")
+
+		req := &proto.Create{Id: int64(id), Name: string(name), Age: int64(age), Salary: int64(salary), Address: string(address)}
+
+		if response, err := client.CreateEmp(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"result": fmt.Sprint(response.Done),
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	})
+}
--- server/main.go
+++ server/main.go
@@ -2,13 +2,97 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"helloWorld/proto"
+	"net"
+
+	//will be used later
+	"database/sql"
+	_ "database/sql"
+
+	//will be used later
+	_ "fmt"
+
+	// import needed for postgrtes
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
 type server struct{}
 
-
 func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		fmt.Printf("Failed to listen to port 4040")
+		panic(err)
+	}
+	srv := grpc.NewServer()
+	proto.RegisterCRUDSreviceServer(srv, &server{})
+	reflection.Register(srv)
 
+	if e := srv.Serve(listener); e != nil {
+		fmt.Printf("Failed to serve on listner")
+		panic(e)
+	}
 }
 
-func (s *server) CreateEmp(ctx context.Context, *create )
+//create emp
+func (s *server) CreateEmp(ctx context.Context, create *proto.Create) (*proto.BoolResult, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		fmt.Printf("in error")
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	test, err := db.Prepare("Insert into COMPANY Values (?)")
+	if err != nil {
+		fmt.Printf("/n Error fetching rown /n")
+		panic(err)
+	}
+	_, e := test.Exec(create.GetId(), create.GetName(), create.GetAge(), create.GetAddress(), create.GetAge())
+	if e != nil {
+		return &proto.BoolResult{Done: false}, e
+	}
+	return &proto.BoolResult{Done: true}, nil
+}
+
+// Implement after confirming client side for create
+func (s *server) RetrieveEmp(ctx context.Context, retrieve *proto.Retrieve) (*proto.Create, error) {
+	// implementation pending
+	return &proto.Create{Id: 1, Name: "TEST", Address: "t", Age: 12, Salary: 12}, nil
+}
+
+func (s *server) UpdateEmp(ctx context.Context, update *proto.Create) (*proto.BoolResult, error) {
+	// implementation pending
+	return &proto.BoolResult{Done: false}, nil
+}
+
+func (s *server) DeleteEmp(ctx context.Context, delete *proto.Retrieve) (*proto.BoolResult, error) {
+	// implementation pending
+	return &proto.BoolResult{Done: false}, nil
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "dbuser"
+	password = "password"
+	dbname   = "test"
+)
+
+var (
+	id      int
+	name    string
+	age     int
+	address string
+	salary  int
+)
